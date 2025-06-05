0399368b96bd57cded61a033189addc7c29dd093
Hamish Downer <hamish@sortitionfoundation.org>
2025-06-05 13:51:09 +0100

    add more error classes from old code

--- src/sortition_algorithms/errors.py
+++ src/sortition_algorithms/errors.py
@@ -4,3 +4,17 @@ class BadDataError(Exception):
 
 class SelectionError(Exception):
     pass
+
+
+class InfeasibleQuotasError(Exception):
+    def __init__(self, quotas: dict[tuple[str, str], tuple[int, int]], output: list[str]):
+        self.quotas = quotas
+        self.output = ["The quotas are infeasible:", *output]
+
+    def __str__(self):
+        return "\n".join(self.output)
+
+
+class InfeasibleQuotasCantRelaxError(Exception):
+    def __init__(self, message: str):
+        self.message = message
