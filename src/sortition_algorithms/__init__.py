a7a2b1e5bb0e94e5e2b25c36df3e3bee68c7fd2a
Hamish Downer <hamish@sortitionfoundation.org>
2025-06-13 17:30:54 +0100

    add a few more names to the module level import

--- src/sortition_algorithms/__init__.py
+++ src/sortition_algorithms/__init__.py
@@ -1,13 +1,22 @@
 """Sortition algorithms for democratic lotteries."""
 
-from sortition_algorithms.core import find_random_sample
+from sortition_algorithms.adapters import CSVAdapter, GSheetAdapter
+from sortition_algorithms.core import (
+    find_random_sample,
+    run_stratification,
+    selected_remaining_tables,
+)
 from sortition_algorithms.features import read_in_features
 from sortition_algorithms.people import read_in_people
 from sortition_algorithms.settings import Settings
 
 __all__ = [
+    "CSVAdapter",
+    "GSheetAdapter",
     "Settings",
     "find_random_sample",
     "read_in_features",
     "read_in_people",
+    "run_stratification",
+    "selected_remaining_tables",
 ]
